b1be70d616e76f36499a31d261dabfef247afe54
David Henning <david.henning@inovex.de>
2023-04-28 21:59:44 +0200

    Add cursor centering and unfolding on next/prev search result

--- astronvim/user/init.lua
+++ astronvim/user/init.lua
@@ -1,7 +1,9 @@
 vim.opt.colorcolumn = "79"
 
 vim.api.nvim_set_keymap('n', '<C-d>', '<C-d>zz', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-u', '<C-u>zz', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-u>', '<C-u>zz', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'n', 'nzzzv', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'N', 'Nzzzv', { noremap = true, silent = true })
 
 return {
   colorscheme = "catppuccin",
