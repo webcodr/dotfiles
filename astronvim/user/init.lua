04d965e9bcf48ec8049c1a463a4af2768b2c63f3
David Henning <david.henning@inovex.de>
2023-04-28 21:03:49 +0200

    Add cursor centering on half-screen vertical movement

--- astronvim/user/init.lua
+++ astronvim/user/init.lua
@@ -1,5 +1,8 @@
 vim.opt.colorcolumn = "79"
 
+vim.api.nvim_set_keymap('n', '<C-d>', '<C-d>zz', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-u', '<C-u>zz', { noremap = true, silent = true })
+
 return {
   colorscheme = "catppuccin",
 
